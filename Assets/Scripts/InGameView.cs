84e5c186efcad276bf4083f481de02872d6934c7
mit1330 <64365341+mittan1330@users.noreply.github.com>
2022-02-15 12:59:06 +0900

    MVP設計への切り替え

--- Assets/Scripts/InGameView.cs
+++ Assets/Scripts/InGameView.cs
@@ -5,12 +5,50 @@ using System;
 public class InGameView : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
+    [SerializeField] private Cell[] cells;
+    public event Action MoveCellsRight;
+    public event Action MoveCellsLeft;
+    public event Action MoveCellsUp;
+    public event Action MoveCellsDown;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveCellsRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveCellsLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveCellsUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveCellsDown();
+        }
+
+    }
 
     public void SetScore(int score)
     {
         scoreText.text = $"Score: {score}";
     }
 
-
+    /// <summary>
+    /// ステージの初期状態をViewに反映
+    /// </summary>
+    public void ApplyStageView(int[,] stageState)
+    {
+        for (var row = 0; row < Const.SquareSize; row++)
+        {
+            for (var col = 0; col < Const.SquareSize; col++)
+            {
+                cells[row * Const.SquareSize + col].SetText(stageState[row, col]);
+            }
+        }
+    }
 
 }
