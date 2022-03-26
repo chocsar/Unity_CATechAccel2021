e9d68084f7ed20a34ce2b9873afe1d59205ec4c8
mit1330 <64365341+mittan1330@users.noreply.github.com>
2022-03-26 10:02:07 +0900

    JSONファイルの読み込み機能を追加

--- Assets/Scripts/Manager/ScoreController.cs
+++ Assets/Scripts/Manager/ScoreController.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class ScoreController : SingletonMonoBehaviour<ScoreController>
 {
+    // ScoreDataに関するJsonのデータ構造を定義
+    [System.Serializable]
+    private struct ScoreDatas
+    {
+        public Score[] scoreDatas;
+    }
+    [System.Serializable]
+    private struct Score
+    {
+        public int recordScore;
+    }
+
+    // ScoreDataを保存するList
+    public List<int> ScoreData;
+
+    private void Start()
+    {
+        // RankingData.jsonをテキストファイルとして読み取り、string型で受け取る
+        string inputString = Resources.Load<TextAsset>("RankingData").ToString();
+        // 上で作成したクラスへデシリアライズ
+        ScoreDatas scoreData = JsonUtility.FromJson<ScoreDatas>(inputString);
+        // ScoreDataのListへJSONのデータを代入
+        for (int scoreCount = 0;scoreCount < scoreData.scoreDatas.Length; scoreCount++)
+        {
+            ScoreData.Add(scoreData.scoreDatas[scoreCount].recordScore);
+        }
+    }
+
     /// <summary> /// PlayerPrefsで保存するキーの名前を列挙型で定義 /// </summary>
     public enum SaveKeyNames
     {
