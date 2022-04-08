215c6286ea2fce6489426c2f2d22c631ec831657
mit1330 <64365341+mittan1330@users.noreply.github.com>
2022-04-08 22:49:08 +0900

    RankElementのテスト実装中

--- Assets/Scripts/ResultScene/ResultSceneView.cs
+++ Assets/Scripts/ResultScene/ResultSceneView.cs
@@ -8,11 +8,27 @@ public class ResultSceneView : MonoBehaviour
     [SerializeField] private Text resultText;
     [SerializeField] private Button retryButton;
 
+    [SerializeField] private RectTransform rankElementPrefab;
+
+    [SerializeField] private RectTransform scrollbarContent;
+
+
+    // C#Action
     public event Action OnClickRetryButton;
 
     private void Start()
     {
         retryButton.onClick.AddListener(() => OnClickRetryButton?.Invoke());
+
+        for(int i = 0; i < 10; i++)
+        {
+            // Itemを生成
+            var item = Instantiate(rankElementPrefab, scrollbarContent);
+            item.GetComponent<RectTransform>().position += new Vector3(0, -i*180, 0);
+
+            // Contentの子として登録  
+        }
+
 
     }
 
